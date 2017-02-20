cb8329862aa61a4c3991c8d246b3ebd3090b5c87
Renat <rzhilkibaev@gmail.com>
2017-02-20 13:58:53 -0800

    0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cfgen",
-    version="0.2.3",
+    version="0.2.4",
     description="Configuration tool for configuration files",
     author="Renat Zhilkibaev",
     author_email="rzhilkibaev@gmail.com",
