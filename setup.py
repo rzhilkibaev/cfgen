5267c0c79a739cfc3ee69e892ade78871753fc8a
Renat <rzhilkibaev@gmail.com>
2017-02-20 14:04:47 -0800

    0.2.5 Fix test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cfgen",
-    version="0.2.4",
+    version="0.2.5",
     description="Configuration tool for configuration files",
     author="Renat Zhilkibaev",
     author_email="rzhilkibaev@gmail.com",
